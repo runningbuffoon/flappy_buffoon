4a0856a21e7b1b282248ba46cc4265bd407a2594
Dorian Grolaux <dorian.grolaux@gmail.com>
2017-10-02 11:32:46 +0200

    Code pour dessiner un cube et le faire sauter (rajouté boucle de rafraîchissement (60fps))

--- main.js
+++ main.js
@@ -1,4 +1,66 @@
+/*jshint enforceall: true, browser: true, devel: true*/
+
 var canvas = document.getElementById('drawCanvas');
 var context = canvas.getContext('2d');
+var keyPresses = [];
 
-context.fillRect(0, 0, 50, 50);
+window.addEventListener('keydown', function (e) {
+	//	if (!keyPresses.includes(e.key)) {
+	keyPresses.push(e.key);
+	//	}
+});
+
+function Position2D(posX, posY) {
+	this.x = posX;
+	this.y = posY;
+}
+
+function Velocity2D(velX, velY) {
+	this.x = velX;
+	this.y = velY;
+}
+
+function Cube(posX, posY, length) {
+	this.position = new Position2D(posX, posY);
+	this.velocity = new Velocity2D(0, 0);
+	this.size = length;
+}
+
+var cube = new Cube(200, 200, 50);
+
+setInterval(runGame, 1000 / 60);
+
+function runGame() {
+	handleKeyPress();
+	handleObjects();
+	drawCanvas();
+}
+
+function handleKeyPress() {
+	for (var keyIndex in keyPresses) {
+		console.log(keyPresses);
+		switch (keyPresses[keyIndex]) {
+			case 'z':
+				jump();
+				break;
+		}
+	}
+
+	keyPresses = [];
+}
+
+function handleObjects() {
+	cube.position.x += cube.velocity.x;
+	cube.position.y += cube.velocity.y;
+
+	cube.velocity.y += 10 / 60;
+}
+
+function drawCanvas() {
+	context.clearRect(0, 0, 800, 600);
+	context.fillRect(cube.position.x, cube.position.y, cube.size, cube.size);
+}
+
+function jump() {
+	cube.velocity.y = -5;
+}
