7c86cc9b80e8e820e36ce68c28e5d76e9f342372
Dorian Grolaux <dorian.grolaux@gmail.com>
2017-10-14 17:03:29 +0200

    Le Painter dessine maintenant tout les objets d'une scène

--- Source/Controllers/Painter.js
+++ Source/Controllers/Painter.js
@@ -7,5 +7,10 @@ function Painter(context) {
 	Painter.prototype.paint = function (scene) {
 		this.context.clearRect(0, 0, 800, 600);
 		this.context.fillRect(scene.actor.position.x, scene.actor.position.y, scene.actor.dimension.width, scene.actor.dimension.height);
-	}
+
+		for (var i in scene.objects) {
+			var object = scene.objects[i];
+			this.context.fillRect(object.position.x, object.position.y, object.dimension.width, object.dimension.height);
+		}
+	};
 }
