4942e72555740b61f5cb70a337097e64165777ac
Dorian Grolaux <dgrolaux@nl.ibm.com>
2017-10-24 15:12:37 +0200

    Modifié méthode pour rajouter des obstacles (possède mtn un objet invisible pour le "trou")

--- Source/Models/ObstacleProvider.js
+++ Source/Models/ObstacleProvider.js
@@ -6,9 +6,9 @@ function ObstacleProvider() {
 	var OBSTACLE_VELOCITY = new Velocity(-2, 0);
 
 	this.getRandomObstaclePair = function (posX, width) {
-		var hole = getRandomHole();
-		var topObstacleHeight = hole.y;
-		var botObstacleHeight = 600 - (hole.y + 150);
+		var hole = getRandomHole(posX, width);
+		var topObstacleHeight = hole.position.y;
+		var botObstacleHeight = 600 - (hole.position.y + 150);
 
 		var topObstacle = new PhysicObject(
 			new Point(posX, 0),
@@ -17,20 +17,23 @@ function ObstacleProvider() {
 		);
 
 		var botObstacle = new PhysicObject(
-			new Point(posX, hole.y + 150),
+			new Point(posX, hole.position.y + 150),
 			OBSTACLE_VELOCITY,
 			new Dimensions(width, botObstacleHeight)
 		);
 
 		return {
 			"topObstacle": topObstacle,
+			"hole": hole,
 			"bottomObstacle": botObstacle
 		};
 	};
 
-	function getRandomHole() {
+	function getRandomHole(posX, width) {
 		var posY = 350 * Math.random();
 
-		return new Point(0, 50 + posY);
+		var object = new PhysicObject(new Point(posX, 50 + posY), OBSTACLE_VELOCITY, new Dimensions(width, 150), new Sprite("clear"));
+		object.hasScored = false;
+		return object;
 	}
 }
