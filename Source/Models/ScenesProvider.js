5c8ac75aad5ffd28b341c2f7710c97d9469d546d
Dorian Grolaux <dgrolaux@nl.ibm.com>
2017-11-09 15:00:02 +0100

    Nettoyage

--- Source/Models/ScenesProvider.js
+++ Source/Models/ScenesProvider.js
@@ -60,59 +60,59 @@ function ScenesProvider() {
 		scene.objects.push(obstacles.hole);
 		scene.objects.push(obstacles.bottomObstacle);
 	}
-}
 
-function handleKeyPress(key) {
-	switch (key) {
-		case 'z':
-			jump(this.actor);
-			break;
+	function handleKeyPress(key) {
+		switch (key) {
+			case 'z':
+				jump(this.actor);
+				break;
 
-		case 'd':
-			moveRight(this.actor);
-			break;
+			case 'd':
+				moveRight(this.actor);
+				break;
 
-		case 'q':
-			moveLeft(this.actor);
-	}
-}
-
-function moveRight(actor) {
-	actor.velocity.x = 10;
-}
-
-function moveLeft(actor) {
-	actor.velocity.x = -10;
-}
-
-function jump(actor) {
-	if (actor.velocity.y >= 0) {
-		actor.velocity.y = -5;
-	} else {
-		actor.velocity.y -= 5;
-	}
-}
-
-function handleActor(cube) {
-	cube.position.x += cube.velocity.x;
-	cube.position.y += cube.velocity.y;
-
-	cube.velocity.x = cube.velocity.x * 0.95;
-
-	if (cube.position.y + cube.dimension.height >= 600) {
-		cube.position.y = 600 - cube.dimension.height;
-		cube.velocity.y = -cube.velocity.y / 1.5;
+			case 'q':
+				moveLeft(this.actor);
+		}
 	}
 
-	if (cube.position.y <= 0) {
-		cube.position.y = 0;
-		cube.velocity.y = -cube.velocity.y / 1.5;
+	function moveRight(actor) {
+		actor.velocity.x = 10;
 	}
 
-	cube.velocity.y += 20 / 60;
-}
+	function moveLeft(actor) {
+		actor.velocity.x = -10;
+	}
 
-function handleObject(object) {
-	object.position.x += object.velocity.x;
-	object.position.y += object.velocity.y;
+	function jump(actor) {
+		if (actor.velocity.y >= 0) {
+			actor.velocity.y = -5;
+		} else {
+			actor.velocity.y -= 5;
+		}
+	}
+
+	function handleActor(cube) {
+		cube.position.x += cube.velocity.x;
+		cube.position.y += cube.velocity.y;
+
+		cube.velocity.x = cube.velocity.x * 0.95;
+
+		if (cube.position.y + cube.dimension.height >= 600) {
+			cube.position.y = 600 - cube.dimension.height;
+			cube.velocity.y = -cube.velocity.y / 1.5;
+		}
+
+		if (cube.position.y <= 0) {
+			cube.position.y = 0;
+			cube.velocity.y = -cube.velocity.y / 1.5;
+		}
+
+		cube.velocity.y += 20 / 60;
+	}
+
+	function handleObject(object) {
+		object.position.x += object.velocity.x;
+		object.position.y += object.velocity.y;
+	}
 }
